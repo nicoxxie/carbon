f481a2d41a44512bf44ec0a84464a322c9d24d9b
Josh Black <joshblack@us.ibm.com>
2018-03-07 15:50:11 -0600

    chore(polyfill): update gitignore to exclude umd

--- packages/carbon-polyfill/react/node/v6.umd.js
+++
@@ -1,9 +0,0 @@
-(function(global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined'
-    ? factory()
-    : typeof define === 'function' && define.amd ? define(factory) : factory();
-})(this, function() {
-  'use strict';
-
-  require('core-js/modules/es7.object.values');
-});
